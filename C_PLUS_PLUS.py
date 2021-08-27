4f3cf5425cefd6a006e547459f223c1ab77dac46
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2021-08-27 18:45:55 +0530

    Added Default CPP Template

--- C_PLUS_PLUS.py
+++ C_PLUS_PLUS.py
@@ -1,4 +1,56 @@
 c_plus_plus_functions = {
+    "Default Template":
+"""
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(x, N)					for(int x = 0; x < N; x++)
+#define inverse(a, p)				power(a, p - 2, p)
+
+typedef unsigned long long int ull;
+typedef long long int ll;
+
+const short int dc[] = {1, 0, 0, -1, -1, -1, 1, 1};
+const short int dr[] = {0, 1, -1, 0, -1, 1, -1, 1};
+
+const ll shit	= ((ll)(998244353));
+const ll mod	= ((ll)(1e9 + 7));
+const ll hell	= ((ll)(1e9 + 9));
+const ll inf	= ((ll)(1e18 + 3));
+
+static inline ll gcd(ll a, ll b) {
+	for(ll rem = 0; b > 0; rem = a % b, a = b, b = rem);
+	return a;
+}
+static inline ll lcm(ll a, ll b) {
+	return (a * b) / gcd(a, b);
+}
+
+#define nax 2000003
+
+
+void pre_compute() {
+	return;
+}
+
+void solve() {
+	return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int t = 0;
+	// t++;
+	if(!t) cin >> t;
+	pre_compute();
+	FOR(test, t) {
+		// cout << "Case #" << (test + 1) << ": ";
+		solve();
+	}
+	return 0;
+}
+""",
     "Binary Search":
 """
 template <typename T>
