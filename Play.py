ac995ebc9ac95f8412ad269f5a79de88251d6614
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2021-09-24 13:01:45 +0530

    gp hash table using policy based ds, modified music player

--- Play.py
+++ Play.py
@@ -7,12 +7,20 @@ out = out.stdout.decode()
 out = list(map(str, out.split('\n')))
 
 song_keys = argv[1:]
-path = ""
+paths = []
 for item in out:
     if all([key.lower() in item.lower() for key in song_keys]):
-        path = item
-        break
+        paths.append(item)
 
-print(os.path.realpath(path))
+if paths == []:
+	print("Song not found")
+	exit(0)
 
-run(['cvlc', os.path.realpath(path)])
+for i in range(len(paths)):
+	print(str(i + 1) + "). " + paths[i])
+
+index = int(input("Enter index: "))
+
+print(os.path.realpath(paths[index - 1]))
+
+run(['vlc', os.path.realpath(paths[index - 1])])
