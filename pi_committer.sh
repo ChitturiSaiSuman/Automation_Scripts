8b91fb290e91b97c3b73647f6eb5432108a73936
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2023-08-11 22:57:25 +0530

    Modified git commit script

--- pi_committer.sh
+++ pi_committer.sh
@@ -10,11 +10,12 @@ read -p 'Commit Message: ' message
 
 git commit -m "$message"
 
-python3 /home/suman/Automation_Scripts/Copy.py /home/suman/Git/Token.txt
+# python3 /home/suman/Automation_Scripts/Copy.py /home/suman/Git/Token.txt
+cat /home/suman/Git/Token.txt
 
 git push
 echo "Uploaded Successfully"
 
-touch empty.txt
-xclip -sel c < empty.txt
-rm empty.txt
+# touch empty.txt
+# xclip -sel c < empty.txt
+# rm empty.txt
